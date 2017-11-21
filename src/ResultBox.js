9303325773948a1fca8ad21736bbbfafff6f32a2
Harit Moolphunt <harit20451@outlook.com>
2017-11-21 22:58:56 +0700

    Start with create-react-app

--- src/ResultBox.js
+++ src/ResultBox.js
@@ -1,7 +1,25 @@
 import React, {Component} from "react"
-import { Table, Container, Row, Col } from 'reactstrap';
+import { Table, Container, Row, Col, TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText} from 'reactstrap';
+import classnames from 'classnames';
 
 class ResultBox extends Component{
+
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            activeTab: '1'
+        };
+    }
+
+    toggle(tab) {
+        if (this.state.activeTab !== tab) {
+            this.setState({
+                activeTab: tab
+            });
+        }
+    }
 
     render(){
         let{
@@ -25,13 +43,36 @@ class ResultBox extends Component{
 
         return(
             <div class="ResultBox">
-                <div class="ResultHead">
-                    <h2>Result</h2>
-                </div>
+
+
             <Container fluid>
                 <Row>
                     <Col></Col>
                     <Col>
+
+                        <div class="ResultHead">
+                            <h2>Result</h2>
+                        </div>
+
+                        <Nav tabs>
+                            <NavItem>
+                                <NavLink
+                                    className={classnames({ active: this.state.activeTab === '1' })}
+                                    onClick={() => { this.toggle('1'); }}
+                                >
+                                    Info
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink
+                                    className={classnames({ active: this.state.activeTab === '2' })}
+                                    onClick={() => { this.toggle('2'); }}
+                                >
+                                    All Possible Networks
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+
             <Table size="sm" hover>
                 <tbody>
                 <tr>
